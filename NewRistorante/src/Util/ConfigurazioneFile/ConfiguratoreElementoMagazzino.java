09a759db83ea087b76c489531c0d0715fb09455c
EsterAlyssa <ester.adinolfi@alice.it>
2023-06-23 18:48:22 +0200

    metà conf El Mag

--- NewRistorante/src/Util/ConfigurazioneFile/ConfiguratoreElementoMagazzino.java
+++ NewRistorante/src/Util/ConfigurazioneFile/ConfiguratoreElementoMagazzino.java
@@ -1,6 +1,10 @@
 package Util.ConfigurazioneFile;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
+
+import Magazzino.ElementoMagazzino;
+import Magazzino.Merce.Merce;
 
 public class ConfiguratoreElementoMagazzino extends ConfiguratoreManager{
 
@@ -10,15 +14,26 @@ public class ConfiguratoreElementoMagazzino extends ConfiguratoreManager{
 
 	@Override
 	void scriviParametriNelFile(Object oggetto, BufferedWriter writer) {
-		// TODO Auto-generated method stub
-		
+		try {
+			writer.write("merce=");
+			writer.newLine();
+			ConfiguratoreManager confMerce = new ConfiguratoreMerce();
+			Merce merce = ((ElementoMagazzino) oggetto ).getMerce();
+			confMerce.scriviParametriNelFile(merce, writer);
+			writer.newLine();
+			writer.write("quantita="+((ElementoMagazzino) oggetto).getQuantita());
+			writer.newLine();
+		} catch (IOException e) {
+			System.out.println("Impossibile salvare l'oggetto piatto");
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void setAttributiOggetto(String nomeOggetto, String nomeAttributo, String valoreAttributo) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 
 }
